1196b962f6fb05801e68ed1cbee443d8ac5c2bc6
hahahalol9 <cmak7543@mail.ru>
2022-02-26 16:39:09 +0300

    DbConnector class added

--- src/main/java/com/example/javaproject/Classes/DbClasses/DbConnector.java
+++ src/main/java/com/example/javaproject/Classes/DbClasses/DbConnector.java
@@ -1,4 +1,47 @@
 package com.example.javaproject.Classes.DbClasses;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbConnector {
+    private final String url ;
+    private final String name;
+    private final String password;
+    private Connection connection;
+
+    public DbConnector(String url,String name,String password){
+        this.url=url;
+        this.name=name;
+        this.password = password;
+    }
+
+    public boolean tryOpenConnection(){
+        boolean isCorrect = true;
+        try {
+            connection = DriverManager.getConnection(url, name, password);
+        }catch(SQLException exception){
+            isCorrect = false;
+        }
+        return isCorrect;
+    }
+
+    public boolean tryCloseConnection(){
+        boolean isCorrect = connection!=null;
+        if(isCorrect){
+            try {
+                connection.close();
+            }catch(SQLException exception){
+                isCorrect = false;
+            }
+        }
+        return isCorrect;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,13 @@
 module com.example.javaproject {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.jsoup;
+    requires  java.sql;
 
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
 
     opens com.example.javaproject to javafx.fxml;
     exports com.example.javaproject;
+    opens packages;
 }
